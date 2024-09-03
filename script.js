2effb1d3dc13827bf5c6f865628665506ff01199
EstefnyMachado <106812507+EstefnyMachado@users.noreply.github.com>
2024-09-02 23:01:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+<script src="script.js"></script>
+
+constcaixa-principal;documento.querySelector('.caixa-principal');
+constcaixa-perguntas;documento.querySelector('.caixa-perguntas');
+constcaixa-alternativa;documento.querySelector('.caixa-alternativa');
+constcaixa-resultado;documento.querySelector('.caixa-resultado');
+constcaixa-resultado;documento.querySelector('.texto-resultado');
+
+constlista:( item1,item2);
+
+constperguntas=[
+    {
+        enunciado "O que é este animal?"
+        alternativa:[
+            "Pato ou Pata"
+            "Avestruz"
+            ""
+        ]
+    }
+]
